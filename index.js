872e379413e1944ae256cf4161a5433bb923623b
Saurabh Singh <saurabh_xcz@yahoo.co.in>
2024-12-07 10:54:41 +0530

    update index

--- index.js
+++ index.js
@@ -24,9 +24,7 @@ const resolvers = {
 
 // server setup
 const server = new ApolloServer({
-    // A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+
     typeDefs,
     resolvers
     
